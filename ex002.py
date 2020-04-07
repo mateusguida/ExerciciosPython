e1a07e7e34c4a9cba16b426b11d75bc85fbd3867
mateusguida <guidamaster@gmail.com>
2020-04-06 22:45:04 -0300

    Ex001 a Ex005
    Primeiros exercícios em Python

---
+++ ex002.py
@@ -0,0 +1,2 @@
+name = input("Digite o seu nome:")
+print(f'Olá {name}, seja bem vindo a aula!')
---
+++ ex003.py
@@ -0,0 +1,4 @@
+num1 = input("Digite um valor: ")
+num2 = input("Digite outro valor: ")
+
+print(f"A soma entre {num1} e {num2} é {int(num1) + int(num2)}!")
