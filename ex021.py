72d11434270a9a314a9083d7dc4f5fc20816d567
mateusguida <guidamaster@gmail.com>
2020-04-17 15:13:02 -0300

    Update ex021.py
    Tentativa de ajeitar o ex021, com problemas na importação de módulos externos... Ainda sem sucesso.

--- ex021.py
+++ ex021.py
@@ -1,5 +1,11 @@
+'''
+# DEFASADO
 import pygame
 pygame.init()
 pygame.mixer.music.load('ex021.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
+'''
+
+from playsound import playsound
+playsound('ex021.mp3')
